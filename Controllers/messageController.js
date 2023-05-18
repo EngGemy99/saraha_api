3530a395af5ff52ee4cff74e6a8605a09feba64f
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-05-18 17:52:21 +0300

    Add files via upload

---
+++ Controllers/messageController.js
@@ -0,0 +1,48 @@
+import messageModel from "../Models/messageModel.js";
+
+const addMessage = async (request, response, next) => {
+  try {
+    await messageModel.insertMany(request.body);
+    response.status(200).json({
+      message: "Send Successfully 👋",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const getMessageUser = async (request, response, next) => {
+  try {
+    let pageNumber = request.query.page;
+    //? if user send string in url
+    pageNumber = pageNumber * 1 || 1;
+    if (pageNumber <= 0 || !pageNumber) pageNumber = 1;
+    let LIMIT = 5;
+    let skip = (pageNumber - 1) * LIMIT;
+    //! catch id from token
+    const receivedId = request.userId;
+    let data = await messageModel
+      .find({ receivedId })
+      .skip(skip)
+      .limit(LIMIT)
+      .sort("-createdAt");
+    response.status(200).json({
+      data,
+      pageNumber,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const deleteMessage = async (request, response, next) => {
+  try {
+    const { id } = request.body;
+    await messageModel.findByIdAndDelete(id);
+    response.status(200).json({
+      message: "Delete Successfully 🧺",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { addMessage, getMessageUser, deleteMessage };
